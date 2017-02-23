05091795add00b22deb693cc2f5619f02a84d4f4
Jaime Parra Jr <jparra@us.ibm.com>
2017-02-23 15:20:19 -0600

    Rename file to reflect the format.

--- build/karma.unit.config.js
+++ build/karma.unit.config.js
@@ -8,7 +8,7 @@ module.exports = function (config) {
     junitReporter: {
       // outputDir is set as below to accumulate test outputs in root directory for accessing easily.
       outputDir: process.cwd(), // results will be saved as $outputDir/$browserName.xml
-      outputFile: "../mochaResults.xml" // if included, results will be saved as $outputDir/$browserName/$outputFile
+      outputFile: "../junitResults.xml" // if included, results will be saved as $outputDir/$browserName/$outputFile
       // suite: '', // suite will become the package name attribute in xml testsuite element
       // useBrowserName: true, // add browser name to report and classes names
       // nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
